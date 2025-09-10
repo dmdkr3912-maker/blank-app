20ad2fe3df496b3c103b9e2399b34e89781bd3a6
dmdkr3912-maker <dmdkr3912@gmail.com>
2025-09-10 08:30:19 +0000

    내용 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Hello 휘")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "2025년 9월 10일 Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
