bf4b4b4705717ebc5a10ed6559a708027a75f305
dmdkr3912-maker <dmdkr3912@gmail.com>
2025-09-11 05:54:26 +0000

    코드 수정01

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("🎈 Hello 휘")
-st.write(
-    "2025년 9월 10일 Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
